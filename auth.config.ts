23100b263d31987077195727dfb7ccb1a55a2b91
Harshit Ahuja <harshitahuja@bluepi.in>
2023-10-31 18:31:21 +0530

    path fix

--- auth.config.ts
+++ auth.config.ts
@@ -1,26 +1,23 @@
 import type { NextAuthConfig } from "next-auth";
 
 export const authConfig = {
-  providers: [],
   pages: {
     signIn: "/login",
   },
+  providers: [
+    // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
+    // while this file is also used in non-Node.js environments
+  ],
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      console.log(auth, "auth");
-      console.log(Request, "request");
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
       if (isOnDashboard) {
-        console.log("inside if");
         if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
+        return false; // Redirect unathenticated users to login page
       } else if (isLoggedIn) {
-        console.log("inside el if");
-
         return Response.redirect(new URL("/dashboard", nextUrl));
       }
-      console.log("otsie if el");
       return true;
     },
   },
--- auth.ts
+++ auth.ts
@@ -28,13 +28,13 @@ export const { auth, signIn, signOut } = NextAuth({
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
-          console.log(user, "user");
+          // console.log(user, "user");
 
           if (!user) return null;
           const passwordsMatch = await bcrypt.compare(password, user.password);
 
           if (passwordsMatch) return user;
-          console.log(user, "user");
+          // console.log(user, "user");
         }
 
         console.log("Invalid credentials");
